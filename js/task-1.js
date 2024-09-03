2dd7b25bdc760a10b5f6bace4f306a4b067b5e83
Anna <harisova.anna11@gmail.com>
2024-09-03 14:56:50 +0300

    add task-1 task-2

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const list = document.querySelector("#categories");
+const categoriesItems = list.querySelectorAll(".item");
+
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach(item => {
+    const title = item.querySelector("h2").textContent;
+    const elements = item.querySelectorAll("ul > li").length;
+
+    console.log(`Category: ${title}`);
+    console.log(`Elements: ${elements}`)
+});
